978676b3603b252947d05cad9e0cffbda4c25764
Ilya Volkov <lyxich@yandex.ru>
2025-05-25 14:45:47 +0300

    comments worked out (2)

--- conftest.py
+++ conftest.py
@@ -1,39 +1,6 @@
-
 import pytest
-import random
-import string
-import requests
+from utils.helpers import register_new_courier, delete_courier
 
-def generate_random_string(length):
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for _ in range(length))
-
-def register_new_courier():
-    login = generate_random_string(10)
-    password = generate_random_string(10)
-    first_name = generate_random_string(10)
-
-    payload = {
-        "login": login,
-        "password": password,
-        "firstName": first_name
-    }
-
-    response = requests.post("https://qa-scooter.praktikum-services.ru/api/v1/courier ", data=payload)
-
-    if response.status_code == 201:
-        return {
-            "login": login,
-            "password": password,
-            "firstName": first_name,
-            "id": response.json()["id"]
-        }
-    else:
-        raise Exception(f"Не удалось зарегистрировать курьера. Код: {response.status_code}, Ответ: {response.text}")
-
-def delete_courier(courier_id):
-    response = requests.delete(f"https://qa-scooter.praktikum-services.ru/api/v1/courier/ {courier_id}")
-    return response
 
 @pytest.fixture(scope="function")
 def registered_courier():
@@ -42,5 +9,5 @@ def registered_courier():
 
     yield courier_data
 
-    # Удаляем курьера после теста
+    # Удаляем курьера после выполнения теста
     delete_courier(courier_data["id"])
